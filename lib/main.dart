d0f7006c993b60b60cf33e1cf40be22ab4b431f4
pdfmagic <seniorlaguna@lagunabrothers.de>
2022-09-23 13:49:39 +0200

    web view ad

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_web_view2/easy_web_view2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -107,6 +108,12 @@ class PDFMagicApp extends StatelessWidget {
                           ),
                         ),
                 ),
+                EasyWebView(
+                  height: 80,
+                  width: 800,
+                  src: "https://pdfmagic.de/ads", onLoaded: () {
+                  print("Page loaded");
+                })
               ],
             );
           },
