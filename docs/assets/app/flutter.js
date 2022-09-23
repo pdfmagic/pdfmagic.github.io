6c323f84ca0cf27d8b193483fc50077f0e358f09
pdfmagic <seniorlaguna@lagunabrothers.de>
2022-09-23 07:05:42 +0200

    t

--- docs/assets/app/flutter.js
+++ docs/assets/app/flutter.js
@@ -42,7 +42,7 @@ _flutter.loader = null;
      */
     loadEntrypoint(options) {
       const {
-        entrypointUrl = "main.dart.js",
+        entrypointUrl = "/assets/app/main.dart.js",
         serviceWorker,
       } = (options || {});
       return this._loadWithServiceWorker(entrypointUrl, serviceWorker);
